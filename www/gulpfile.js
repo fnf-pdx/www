343a6fa2739e1c2d2a43eed4ee95b429750aae73
notxt <michaelkschoenfelder@gmail.com>
2015-08-20 22:53:33 -0700

    templates compiling from gulp task; still solution for images

--- www/gulpfile.js
+++ www/gulpfile.js
@@ -1,10 +1,36 @@
 var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
+var browserSync = require('browser-sync');
+var sass        = require('gulp-sass');
+var swig        = require('gulp-swig');
+var reload      = browserSync.reload;
 
-gulp.task('browser-sync', function() {
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    }
+var src = {
+  scss: 'scss/*.scss',
+  html: 'templates/*.html'
+};
+
+gulp.task('serve', ['sass'], function() {
+
+  browserSync({
+    server: "./dist"
   });
+
+  gulp.watch(src.scss, ['sass']);
+  gulp.watch(src.html, ['templates']);
 });
+
+gulp.task('templates', function() {
+  return gulp.src(src.html)
+    .pipe(swig())
+    .pipe(gulp.dest('./dist'))
+    .on("end", reload);
+});
+
+gulp.task('sass', function() {
+  return gulp.src(src.scss)
+    .pipe(sass())
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('default', ['serve']);
