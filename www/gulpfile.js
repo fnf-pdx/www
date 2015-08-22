ca5f49e36a30aad5afaac1b985917abf24c64df1
notxt <michaelkschoenfelder@gmail.com>
2015-08-22 16:58:18 -0700

    got links working in browser-sync

--- www/gulpfile.js
+++ www/gulpfile.js
@@ -8,22 +8,24 @@ var reload      = browserSync.reload;
 
 var src = {
   scss: 'scss/*.scss',
-  html: 'templates/*.html'
+  html: 'templates/*.html',
+  data: 'mock_data/*.json'
 };
 
 function getJsonData(file) {
-  var data = require('./mock_data/' + path.basename(file.path) + '.json');
-  return data;
+  return require('./mock_data/' + path.basename(file.path) + '.json');
 }
 
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['sass', 'templates'], function() {
 
   browserSync({
-    server: "./dist"
+    server: "./dist",
+    extensions: "html"
   });
 
   gulp.watch(src.scss, ['sass']);
   gulp.watch(src.html, ['templates']);
+  gulp.watch(src.data, ['templates']);
 });
 
 gulp.task('templates', function() {
