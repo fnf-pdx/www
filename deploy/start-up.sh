dba7394c0d5fb6e15a6699afeea5d2b326e31448
notxt <michaelkschoenfelder@gmail.com>
2015-08-24 15:09:34 -0700

    moved deploy and admin to their own repos

--- deploy/start-up.sh
+++
@@ -1,8 +0,0 @@
-yum install git -y
-
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.25.4/install.sh | bash
-
-npm install -g pm2 bower
-
-mkdir /var/fnf
-chown ec2-user:ec2-user /var/fnf
