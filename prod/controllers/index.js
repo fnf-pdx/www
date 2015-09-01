028f3028b0134333fe8123a687432d6aaca4b2a6
notxt <michaelkschoenfelder@gmail.com>
2015-09-01 00:01:50 -0700

    changes from last night

--- prod/controllers/index.js
+++ prod/controllers/index.js
@@ -2,8 +2,6 @@
 
 module.exports = function(schedule) {
   return function(request, reply) {
-    var data = require('../../src/data/index');
-
-    return reply.view('index', data);
+    return reply.view('index', {});
   };
 };
